32158d739ed09dd4636895f3a2ca20be83a0adfd
Jose <joseaguilarsalazar2004@gmail.com>
2025-06-12 22:40:35 -0500

    added admin models

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import (User, Especialidad, Medico, Cita, Paciente, Historial, Pagos, Tratamiento)
+from .models import (Especialidad, Medico, Cita, Paciente, Historial, Pagos, Tratamiento)
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Especialidad)
 admin.site.register(Medico)
 admin.site.register(Paciente)
