123e6c911792d972a3784de25d3808128bf081a8
Jose <joseaguilarsalazar2004@gmail.com>
2025-06-12 22:23:11 -0500

    first commit

--- create_superuser.py
+++ create_superuser.py
@@ -13,7 +13,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-if not User.objects.filter(username='admin').exists():
+if not User.objects.filter(email='admin@gmail.com').exists():
     User.objects.create_superuser(
         email='admin@gmail.com',
         password='1234'
