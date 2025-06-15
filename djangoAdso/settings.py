3123c6d8c649608cae2e4e407abc0f29c811a8a8
Jose <joseaguilarsalazar2004@gmail.com>
2025-06-15 17:27:43 -0500

    now with postgre sql  connection

--- djangoAdso/settings.py
+++ djangoAdso/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'djangoAdso.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adsodb',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'flask_pg-db',  
+        'PORT': '5432',   
     }
 }
 
