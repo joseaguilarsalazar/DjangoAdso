a0eee7d76789fa704dcb5bed50902e2479bf19af
Jose <joseaguilarsalazar2004@gmail.com>
2025-06-29 16:30:24 -0500

    added image upload to s3 minIO

--- odontograma/admin.py
+++ odontograma/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import (
+    Diente,
+    DienteOdontograma,
+    Odontograma,
+    CasoMultidental,
+)
 # Register your models here.
+admin.site.register(Diente)
+admin.site.register(DienteOdontograma)
+admin.site.register(Odontograma)
+admin.site.register(CasoMultidental)
