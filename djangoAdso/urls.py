069f40740dfd610b87ce1708f900cb790c407800
Jose <joseaguilarsalazar2004@gmail.com>
2025-06-29 16:51:34 -0500

    added image upload to s3 minIO

--- djangoAdso/urls.py
+++ djangoAdso/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
     # Tu API
     path('api/', include('core.urls')),
     path('api/', include('accounts.urls')),
+    path('api/', include('odontograma.urls')),
 
     # ✅ Swagger and Redoc URLs<
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
