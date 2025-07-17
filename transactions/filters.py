e9db4f14209c1a45fce347a26c50fce8011f9afd
Jose <joseaguilarsalazar2004@gmail.com>
2025-07-16 22:55:12 -0500

    feat: Added a fucking lot of stuff, dont even remember all of it

--- transactions/filters.py
+++ transactions/filters.py
@@ -8,7 +8,6 @@ class PagosFilter(django_filters.FilterSet):
     class Meta:
         model = Pagos
         fields = {
-            'pacienteTratamiento': ['exact'],
             'paciente': ['exact'],
             'monto': ['gte', 'lte'],
             'created_at': ['date__gte', 'date__lte'],
