1e4fef424f55bf54b44ba9cde6999085b92f3af4
Jose <joseaguilarsalazar2004@gmail.com>
2025-08-21 17:02:08 -0500

    feat: modified cita model, also added filters for clinica in viewsets, and added celery tasks in core

--- create_superuser.py
+++ create_superuser.py
@@ -1,7 +1,6 @@
 # create_superuser.py
 import os
 import django
-from core.models import Clinica
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoAdso.settings")
 django.setup()
@@ -10,14 +9,6 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-clinicaIquitos, created = Clinica.objects.get_or_create(
-            nomb_clin='Clinica Dental Sede Iquitos',
-            defaults={
-                'direc_clin': 'Calle Callao 176',
-                'telf_clin': 917435154,
-                'email_clin': 'email 1',
-            }
-        )
 
 if not User.objects.filter(email='admin@gmail.com').exists():
     User.objects.create_superuser(
