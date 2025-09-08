1af7c0c8e9823250c423b3eaa3597d2ebcaf3c33
Jose <joseaguilarsalazar2004@gmail.com>
2025-09-07 22:35:35 -0500

    feat: added todayScheduleApi

--- core/views/coreViewsets.py
+++ core/views/coreViewsets.py
@@ -179,7 +179,7 @@ class CitaViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_class = CitaFilter
     ordering_fields = '__all__'
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
 
     def get_queryset(self):
         user = self.request.user
