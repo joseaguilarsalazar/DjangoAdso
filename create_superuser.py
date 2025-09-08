17f279cddaa4deb96cf541299c3d98add662b6b5
Jose <joseaguilarsalazar2004@gmail.com>
2025-09-07 22:48:06 -0500

    feat: added todayScheduleApi

--- create_superuser.py
+++ create_superuser.py
@@ -13,7 +13,7 @@ User = get_user_model()
 if not User.objects.filter(email='admin@gmail.com').exists():
     User.objects.create_superuser(
         email='admin@gmail.com',
-        password='1234',
+        password='adso_dental_2025',
         tipo_doc='DNI',
         num_doc='00000001',
         name='Administrador General',
