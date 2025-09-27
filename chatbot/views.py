ee940aeebe6a657f83623b143676c50cd0744975
Jose <joseaguilarsalazar2004@gmail.com>
2025-09-27 14:52:26 -0500

    feat: TratamientoPaciente ahora mustra mas detalles

--- chatbot/views.py
+++ chatbot/views.py
@@ -41,6 +41,8 @@ class WhatsAppWebhookView(APIView):
             return Response({"error": "Invalid payload"}, status=status.HTTP_400_BAD_REQUEST)
         
         sender = sender.split('@')[0] # WhatsApp number
+        if "-" in sender:  # group chats have a hyphen
+            return Response({"status": "ignored (group chat)"}, status=status.HTTP_200_OK)
 
         chat, created = Chat.objects.get_or_create(number=sender)
         user_message = Message.objects.create(chat=chat, text=text, from_user =True)
