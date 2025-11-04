bc6d71a23afb7f24a5f6a1a6e73200fd413d1cd2
Jose <joseaguilarsalazar2004@gmail.com>
2025-11-04 14:05:43 -0500

    added docker compose

--- chatbot/orchestrator/flows/__init__.py
+++ chatbot/orchestrator/flows/__init__.py
@@ -1,9 +1,9 @@
-from data_confirmation import data_confirmation
-from default_chat import default_chat
-from lookup_apointment import lookup_appointment
-from register_appointment import register_appointment
-from register_patient import register_patient
-from lookup_patient import lookup_patient
+from .data_confirmation import data_confirmation
+from .default_chat import default_chat
+from .lookup_apointment import lookup_appointment
+from .register_appointment import register_appointment
+from .register_patient import register_patient
+from .lookup_patient import lookup_patient
 
 __all__ = [
     "data_confirmation",
