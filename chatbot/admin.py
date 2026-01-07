7a34722c66cba03780df7cf3d1ab6bf0f63abd1b
Jose <joseaguilarsalazar2004@gmail.com>
2026-01-07 00:15:07 -0500

    updated template sending, it now uses whatsapp API directly

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Chat, EncuestaSatisfaccion
 
+admin.site.register(Chat)
+admin.site.register(EncuestaSatisfaccion)
 # Register your models here.
