0be2602366a8c5c6d6bf36061f3e35aa46ad8f80
Jose Aguilar <joseaguilarsalazar2004@gmail.com>
2026-01-28 18:34:02 -0500

    updated ingreso egreso stat view

--- core/models.py
+++ core/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models import Q
-from django.conf import settings
 from django.contrib.auth.base_user import BaseUserManager
 
-from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinValueValidator, MaxValueValidator
 
