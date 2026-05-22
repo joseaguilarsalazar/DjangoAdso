663a5e8d142ec6e48dc36e29a94dc76c49465743
Jose Aguilar <joseaguilarsalazar2004@gmail.com>
2026-05-22 16:56:00 -0500

    Update celery.py

--- djangoAdso/celery.py
+++ djangoAdso/celery.py
@@ -5,4 +5,4 @@ from celery.schedules import crontab
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoAdso.settings")  # change if needed
 app = Celery("djangoAdso")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+#app.autodiscover_tasks()
