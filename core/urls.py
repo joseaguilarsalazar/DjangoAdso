a9d80cc552a115e9fbca36936d158afde816be7a
Jose Aguilar <joseaguilarsalazar2004@gmail.com>
2026-05-22 16:41:16 -0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -41,6 +41,6 @@ urlpatterns = [
     path('tratamiento-statistics/', TratamientoStatisticsApiView.as_view()),
     path('recent-patients/', RecentPatientsCountView.as_view()),
     path('trigger-survey-broadcast/', TriggerSurveyBroadcastView.as_view()),
-    path('encuesta-status/', #EncuestaStatusView.as_view()),
+   # path('encuesta-status/', #EncuestaStatusView.as_view()),
     path('appointments_by_doctor/', AppointmentsByDoctorApiView.as_view()),
 ]
