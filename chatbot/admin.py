f0031a5f41af30732d7cf63ab2fe0d12b26fff12
Jose <joseaguilarsalazar2004@gmail.com>
2026-05-29 17:30:53 -0500

    updated all

--- chatbot/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Chat, EncuestaSatisfaccion
-
-admin.site.register(Chat)
-admin.site.register(EncuestaSatisfaccion)
-# Register your models here.
--- chatbot/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import WhatsAppWebhookView
-
-urlpatterns = [
-    path("webhook/", WhatsAppWebhookView.as_view(), name="whatsapp-webhook"),
-]
